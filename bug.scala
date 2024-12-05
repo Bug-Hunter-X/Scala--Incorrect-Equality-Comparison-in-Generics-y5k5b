6ccad73a45a5d287c4994923402195e266a8bcab
BugHunter-X <askzenai@gmail.com>
2024-12-05 23:54:06 +0100

    Add bug.scala file

---
+++ bug.scala
@@ -0,0 +1,23 @@
+```scala
+class MyClass[T](val value: T) {
+  def myMethod(x: T): T = {
+    if (x == value) {
+      return x
+    } else {
+      throw new Exception("Values are not equal")
+    }
+  }
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val myObj = new MyClass[String]("hello")
+    try {
+      println(myObj.myMethod("hello"))
+      println(myObj.myMethod("world"))
+    } catch {
+      case e: Exception => println(e.getMessage)
+    }
+  }
+}
+```
